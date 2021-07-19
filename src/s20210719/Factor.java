0dcda8ac37cd684d770bf30d7d3a1a1a969b13e3
geonwoolee <yung65322136@gmail.com>
2021-07-19 17:55:16 +0900

    약수찾기, 2016 요일찾기

---
+++ src/s20210719/Factor.java
@@ -0,0 +1,53 @@
+package s20210719;
+
+import java.util.ArrayList;
+
+public class Factor {
+	
+	public static void main(String[] args) throws Exception {
+		
+		//43
+//		int left = 13;
+//		int right = 17;
+		
+		//52
+		int left = 24;
+		int right = 27;
+		
+		
+		System.out.println(solution(left, right));
+		
+	}
+	
+	
+	 static public int solution(int left, int right) {
+        int answer = 0;
+        
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i=left; i<=right; i++) {
+        	list.add(i);
+        }
+        
+        for(int i : list) {
+        	int factor = 1;
+        	int cnt = 2;
+        	while (i >= cnt) {
+				if(i % cnt == 0) {
+					factor++;
+				}
+				cnt++;
+			}
+        	if(factor % 2 == 0) {
+        		answer += i;
+        	}else {
+        		answer -= i;
+        	}
+        	
+        }
+        
+        return answer;
+        
+	    
+	 }
+	
+}

8def89c6cf08cd7a3d31b536f77df6afa24eccae
geonwoolee <yung65322136@gmail.com>
2021-08-04 16:58:02 +0900

    Update GymSuit.java

--- src/s20210720/GymSuit.java
+++ src/s20210720/GymSuit.java
@@ -1,15 +1,15 @@
 package s20210720;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class GymSuit {
 	
 	public static void main(String[] args) throws Exception {
 
 		//5
-		int n = 5;
-		int[] lost = {2, 4};
-		int[] reserve = {1, 3, 5};
+//		int n = 5;
+//		int[] lost = {2, 4};
+//		int[] reserve = {1, 3, 5};
 		
         //4
 //		int n = 5;
@@ -17,29 +17,40 @@ public class GymSuit {
 //		int[] reserve = {3};
 			
 		//2
-//		int n = 3;
-//		int[] lost = {3};
-//		int[] reserve = {1};
-			
+		int n = 3;
+		int[] lost = {3};
+		int[] reserve = {1};
 		
-		System.out.println(solution(n, lost, reserve));
-		
-		
+		System.out.println("´ä : "+solution(n, lost, reserve));
 	}
 	
-	//11,2,33,4,55
 	 public static int solution(int n, int[] lost, int[] reserve) {
-		 	int answer = 0;
+		 	int answer = n-lost.length;
 		 	
-		 	ArrayList<Integer> list = new ArrayList<Integer>();
-		 	for(int i=1; i<(n+1); i++) {
-		 		list.add(i);
+		 	Arrays.sort(lost);
+		 	Arrays.sort(reserve);
+		 	
+		 	for(int i=0; i<lost.length; i++){
+	            for(int j=0; j<reserve.length; j++){
+	                if(lost[i] == reserve[j]){
+	                    answer++;
+	                    lost[i] = -1;
+	                    reserve[j] = -1; 
+	                    break; 
+	                }
+	            }
+	        }
+		 	
+		 	for(int i=0; i<lost.length; i++){
+	            for(int j=0; j<reserve.length; j++){
+	            	if((reserve[j]-1) == lost[i] || (reserve[j]+1) == lost[i]) {
+	            		answer++;
+	                    lost[i] = -1;
+	                    reserve[j] = -1; 
+	                    break; 
+	            	}
+	            }
 		 	}
-		 	
-		 	for(int i=0;i<lost.length;i++) {
-		 		list.set(lost[i], 0);
-		 	}
-		 	
 		 	
 		 	return answer;
 	    }

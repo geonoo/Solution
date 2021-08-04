85bfb7e6b23a8fb9cb86c7e55ceba6e93a017fad
geonwoolee <yung65322136@gmail.com>
2021-08-04 17:52:21 +0900

    3문제
    간격, 직사각형, 행렬

---
+++ src/s20210804/IntervalXNumber.java
@@ -0,0 +1,20 @@
+package s20210804;
+
+public class IntervalXNumber {
+		
+	    public static void main(String[] args) {
+	    	solution(10000000,10);
+	    }
+	    
+	    static public long[] solution(int x, int n) {
+	        long[] answer = new long[n];
+	        
+	        for(int i=0;i<n;i++) {
+	        	answer[i] = x*i+x;
+	        	System.out.println(answer[i]);
+	        }
+	        
+	        return answer;
+	    }
+	}
+	
---
+++ src/s20210804/RectangleStar.java
@@ -0,0 +1,21 @@
+package s20210804;
+
+import java.util.Scanner;
+
+	public class RectangleStar {
+		
+	    public static void main(String[] args) {
+	    	
+	        Scanner sc = new Scanner(System.in);
+	        int a = sc.nextInt();
+	        int b = sc.nextInt();
+	        
+	        for (int i=0; i<b; i++) {
+	        	if(i!=0) System.out.println("");
+				for(int j=0;j<a; j++) {
+					System.out.print("*");
+				}
+			}
+	    }
+	}
+	

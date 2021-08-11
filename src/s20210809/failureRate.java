bb3f5fa7050fd95f7d6dffbe07c29be0222d5084
geonwoolee <yung65322136@gmail.com>
2021-08-11 17:11:38 +0900

    실패율

--- src/s20210809/failureRate.java
+++ src/s20210809/failureRate.java
@@ -1,7 +1,6 @@
 package s20210809;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class failureRate {
 		
@@ -9,38 +8,45 @@ public class failureRate {
 //    	5	[2, 1, 2, 6, 2, 4, 3, 3]	[3,4,2,1,5]
 //		4	[4,4,4,4,4]	[4,1,2,3]
     	
-    	int[] stages = {2, 1, 2, 6, 2, 4, 3, 3};
-    	int[] rtn = solution(5, stages); 
-    	for (int i = 0; i < rtn.length; i++) {
-//			System.out.println(rtn[i]);
-		}
+    	int[] stages = {1,2,3,2,2,1,3,2};
+    	System.out.println(Arrays.toString(solution(8, stages))); 
     }
     
     static public int[] solution(int N, int[] stages) {
     	
-    	Arrays.sort(stages);
-    	
-        int[] answer = new int[stages[stages.length-1]-1];
-        ArrayList<Double> rat = new ArrayList<Double>();
-        ArrayList<Integer> rank = new ArrayList<Integer>();
-        
+        int[] answer = new int[N];
+        Map<Integer,Double> map = new HashMap<>();
+
         for (int i = 0; i <N; i++) {
-        	int st = 0;
-        	int user = 0;
+        	double user = 0;
+        	double stage = 0;
         	for (int j = 0; j < stages.length; j++) {
-				if((i+1) <= stages[j]) {
-					if((i+1) == stages[j]) {
-						user++;
-					}
-					st++;
-				}
+        		if((i+1) <= stages[j]) {
+        			stage++;
+        		}
+        		if( (i+1) == stages[j]) {
+        			user++;
+        		}
 			}
-        	rat.add((double)user/(double)st);
-        	rank.add(0);
-        	System.out.println((double)user/(double)st);
+        	
+        	if(user == 0 && stage == 0) stage = 1;
+        	
+        	map.put(i+1, user/stage);
         }
         
-        
+        for (int i = 0; i < N; i++) {
+        	double max = -1;
+            int maxKey = 0;
+            for(Integer key : map.keySet()) {
+                if(max < map.get(key)) {
+                    max = map.get(key);
+                    maxKey = key;
+                }
+            }
+            answer[i] = maxKey;
+            map.remove(maxKey);
+        }
+
         
         return answer;
     }

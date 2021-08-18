ba6e0cda4d66393d43be17eab8bb2322d92194ed
geonwoolee <yung65322136@gmail.com>
2021-08-18 11:40:43 +0900

    level 1문제 clear
    level 1문제 clear

---
+++ src/s20210818/DotProduct.java
@@ -0,0 +1,26 @@
+package s20210818;
+
+public class DotProduct {
+		
+    public static void main(String[] args) {
+    	
+//    	[1,2,3,4]	[-3,-1,0,2]	3
+//    	[-1,0,1]	[1,0,-1]	-2
+    	int[] a = {-1,0,1};
+    	int[] b = {1,0,-1};
+    	System.out.println(solution(a, b));
+    }
+    
+    static public int solution(int[] a, int[] b) {
+        int answer = 0;
+        
+        for (int i = 0; i < a.length; i++) {
+			answer += a[i]*b[i];
+		}
+        
+        return answer;
+    }
+	    
+	    
+}
+	

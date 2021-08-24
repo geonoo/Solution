589b9035321f6d5b979597588f30cbb4059d505c
geonwoo <39722357+geonoo@users.noreply.github.com>
2021-08-24 21:43:11 +0900

    doit
    연습문제 1

---
+++ src/s20210824/FileNameSort_kakao.java
@@ -0,0 +1,19 @@
+package s20210824;
+
+import java.util.Arrays;
+
+public class FileNameSort_kakao {
+		
+    public static void main(String[] args) {
+    	String[] a = {};
+    	System.out.println(Arrays.toString(solution(a)));
+    }
+    
+    static public String[] solution(String[] files) {
+        String[] answer = {};
+        return answer;
+    }
+	    
+    
+}
+	

f7a83a55047a074f38a5148eff77a592379ffb7f
geonwoo <39722357+geonoo@users.noreply.github.com>
2021-08-31 17:31:02 +0900

    사진찍기 문제 등
    백준 알고리즘 2문제
    카카오 1문제

---
+++ src/s20210830/BaekJoon_1.java
@@ -0,0 +1,18 @@
+package s20210830;
+
+public class BaekJoon_1 {
+	public static void main(String[] args) {
+//		|\_/|
+//		|q p|   /}
+//		( 0 )"""\
+//		|"^"`    |
+//		||_/=\\__|
+		
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+		
+	}
+}
---
+++ src/s20210830/BaekJoon_2.java
@@ -0,0 +1,18 @@
+package s20210830;
+
+import java.util.*;
+
+public class BaekJoon_2 {
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double a = sc.nextInt();
+		double b = sc.nextInt();
+		
+		
+		System.out.println(a/b);
+		
+	}
+}

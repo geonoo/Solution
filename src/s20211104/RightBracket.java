9b715da3ff6f6f52b8101a9d5bb605231c2a730c
geonwoo <39722357+geonoo@users.noreply.github.com>
2021-11-04 14:41:49 +0900

    올바른 괄호열

---
+++ src/s20211104/RightBracket.java
@@ -0,0 +1,35 @@
+package s20211104;
+
+public class RightBracket {
+
+	public static void main(String[] args) {
+		System.out.println(solution("())"));
+	}
+	
+	static boolean solution(String s) {
+        boolean answer = true;
+        
+        int cnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+        	
+        	if(cnt < 0) {
+        		answer = false;
+        		break;
+        	}
+        	
+			if(s.charAt(i) == ')') {
+				cnt--;
+			}else {
+				cnt++;
+			}
+		}
+        
+        if(cnt == 0) {
+        	answer = true;
+        }else {
+        	answer = false;
+        }
+        return answer;
+    }
+	
+}

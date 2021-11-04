49b8f588bb71240058f18e69d36f8a4604fc0a98
geonwoo <39722357+geonoo@users.noreply.github.com>
2021-11-04 14:51:05 +0900

    Create NoneNumPlus.java

---
+++ src/s20211104/NoneNumPlus.java
@@ -0,0 +1,20 @@
+package s20211104;
+
+public class NoneNumPlus {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	static public int solution(int[] numbers) {
+        
+		int temp = 0;
+        for (int i = 0; i < numbers.length; i++) {
+			temp += numbers[i];
+		}
+        
+        return 45-temp;
+    }
+
+}

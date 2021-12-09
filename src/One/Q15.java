4a33946a8e552e3497a6e201ac03fc2a316d6006
geonwoo <39722357+geonoo@users.noreply.github.com>
2021-12-09 15:04:44 +0900

    DoIt
    1장!

---
+++ src/One/Q15.java
@@ -0,0 +1,61 @@
+package One;
+
+public class Q15 {
+
+	public static void main(String[] args) {
+		trianglelB(3);
+	}
+	
+	static void trianglelB(int n) {
+		
+		for (int i = 1; i <=n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+	static void trianglelU(int n) {
+		
+		for (int i = n; i > 0; i--) {
+			for (int j = i; j > 0; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+	static void trianglelRU(int n) {
+		String a = "";
+		String b = "";
+		for (int i = n; i > 0; i--) {
+			a = "";
+			for (int j = i; j > 0; j--) {
+				a += "*";
+			}
+			a = b+a;
+			System.out.print(a);
+			b += " ";
+			System.out.println();
+			
+		}
+	}
+	
+	static void trianglelRB(int n) {
+		for (int i = 1; i <= n; i++) {
+			String star = "";
+			for (int j = 1; j <= i; j++) {
+				star += "*";
+			}
+			String blank = "";
+			for(int j=0; j<(n-i);j++) {
+				blank += " ";
+			}
+			System.out.print(blank+star);
+			System.out.println();
+			
+		}
+	}
+	
+}
---
+++ src/One/Q16.java
@@ -0,0 +1,26 @@
+package One;
+
+public class Q16 {
+
+	public static void main(String[] args) {
+		spira(4);
+	}
+	
+	static void spira(int n) {
+		for (int i = 1; i <=n; i++) {
+			String star = "";
+			String blank = "";
+			for (int j = 1; j <= i; j++) {
+				star += "*";
+			}
+			for (int j = i; j > 1; j--) {
+				star += "*";
+			}
+			for (int j = i; j < n+1; j++) {
+				blank += " ";
+			}
+			System.out.println(blank+star);
+		}
+	}
+
+}

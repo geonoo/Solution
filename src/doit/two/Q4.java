75e0c46f6837e21d8b517bf4e2f4a8d65306218b
geonwoo <39722357+geonoo@users.noreply.github.com>
2021-12-16 12:44:10 +0900

    배열
    문제 4~5
    기수변환

---
+++ src/doit/two/Q4.java
@@ -0,0 +1,32 @@
+package doit.two;
+
+import java.util.Arrays;
+
+public class Q4 {
+	
+	static int[] a = {1,2,3,4,5};
+	static int[] b = new int[a.length];
+	
+	public static void main(String[] args) {
+		
+//		copy(a, b);
+		rCopy(a, b);
+		
+	}
+	
+	static void copy(int[] a, int[] b) {
+		b = a.clone();
+		
+		System.out.println(Arrays.toString(b));
+	}
+	
+	static void rCopy(int[] a, int[] b) {
+		for (int i = b.length-1; i >= 0; i--) {
+			b[b.length-1-i] = a[i];
+		}
+		
+		System.out.println(Arrays.toString(b));
+		
+	}
+	
+}

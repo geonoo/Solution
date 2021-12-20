aaa3b84d5063165a2dc569dbba69ef9286a76a25
geonwoo <39722357+geonoo@users.noreply.github.com>
2021-12-20 19:57:51 +0900

    기능개발
    try 2

--- src/s20211220/기능개발.java
+++ src/s20211220/기능개발.java
@@ -17,7 +17,39 @@ public class 기능개발 {
 		
 	}
 	
-	static public int[] solution(int[] progresses, int[] speeds) {
+	static public int[] solution (int[] progresses, int[] speeds) {
+		List<Integer> answer = new ArrayList<Integer>();
+		int[] day = new int[speeds.length];
+        
+        for(int i = 0; i < speeds.length; i++){
+            day[i] = (100 - progresses[i]) / speeds[i];
+            if ((100 - progresses[i]) % speeds[i] != 0){
+                day[i] += 1;
+            }
+        }
+		
+        int prev = day[0];
+        int count = 1;
+        for (int i = 1; i < day.length; i++) {
+			if(prev < day[i]) {
+				answer.add(count);
+				count = 1;
+				prev = day[i];
+			}else {
+				count++;
+			}
+		}
+        answer.add(count);
+		
+		int[] rtn = new int[answer.size()];
+        for (int i = 0; i < rtn.length; i++) {
+			rtn[i] = answer.get(i);
+		}
+        
+        return rtn;
+	}
+	
+	static public int[] solution_fail (int[] progresses, int[] speeds) {
 		
 		List<Integer> answer = new ArrayList<Integer>();
 		

d1e43ee685dbe8248c668cd394f3b9db25259ad9
geonwoo <39722357+geonoo@users.noreply.github.com>
2021-12-23 16:23:55 +0900

    선형검색
    문제3

---
+++ src/doit/c_three/Q3.java
@@ -0,0 +1,48 @@
+package doit.c_three;
+
+import java.util.Scanner;
+
+public class Q3 {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.print("요솟수 : ");
+		int num = stdIn.nextInt();
+		int[] x = new int[num + 1];
+		
+		for (int i = 0; i < num; i++) {
+			System.out.print("x[" + i + "] : ");
+			x[i] = stdIn.nextInt();
+		}
+		
+		System.out.print("검색할 값 : ");
+		int ky = stdIn.nextInt();
+		
+		int[] idx = sameKey(x, num, ky);
+		
+		System.out.println("같은 키의 개수 : " + idx.length );
+		
+	}
+	
+	static int[] sameKey(int[] x, int num, int ky) {
+		
+		int rtn = 0;
+		String r = "";
+		
+		for (int i = 0; i < x.length; i++) {
+			if(ky == x[i]) {
+				r += i+"";
+				rtn++;
+			}
+		}
+		
+		int[] arr = new int[rtn];
+		for (int i = 0; i < rtn; i++) {
+			arr[i] = Integer.parseInt(r.charAt(i)+"");
+		}
+		
+		return arr;
+	}
+
+}

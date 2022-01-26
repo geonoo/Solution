187e2aff703f0249e5145d15475d3521758ddc6c
geonwoo <39722357+geonoo@users.noreply.github.com>
2022-01-26 20:56:29 +0900

    프로그래머스
    신고결과받기 L2

---
+++ src/Programmers/신고결과받기.java
@@ -0,0 +1,17 @@
+package Programmers;
+
+public class 신고결과받기 {
+
+	public static void main(String[] args) {
+		 
+	}
+	
+	public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
+        
+        
+        
+        return answer;
+    }
+
+}

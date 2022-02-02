bf664b5eb68736388f6b98dc256baf6bc53474b1
geonwoo <39722357+geonoo@users.noreply.github.com>
2022-02-03 07:36:29 +0900

    삽입 정렬

---
+++ src/doit/chap06/InsertionSortQ7.java
@@ -0,0 +1,51 @@
+package doit.chap06;
+
+import java.util.Scanner;
+
+public class InsertionSortQ7 {
+	
+	static void inserttionSort(int[] a, int n) {
+		for (int i = 2; i < n; i++) {
+			int j;
+			int tmp = a[i];
+			for (j = i; j > 0 && a[j-1] > tmp; j--) {
+				a[j] = a[j-1];
+			}
+			a[j] = tmp;
+		}
+	}
+	
+	//배열의 길이를 +1 해야함 
+	static void insertionSort2(int[] a, int n) {
+		for (int i = 2; i < n; i++) {
+			int tmp = a[0] = a[i];
+			int j = i;
+			for (; a[j - 1] > tmp; j--)
+				a[j] = a[j - 1];
+			if (j > 0)
+				a[j] = tmp;
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("단순 삽입 정렬 ");
+		System.out.print("요솟수 : ");
+		int nx = sc.nextInt();
+		int[] x = new int[nx];
+		
+		for (int i = 0; i < x.length; i++) {
+			System.out.print("x[" +i+ "] : ");
+			x[i] = sc.nextInt();
+		}
+		
+		inserttionSort(x, nx);
+		
+		System.out.println("오름차순 정렬 ok");
+		for (int i = 0; i < x.length; i++) {
+			System.out.print(x[i] + " ");
+		}
+	}
+
+}

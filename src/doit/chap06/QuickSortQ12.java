2f2e369d6e4ad8c2085d908d97c9a1cbb0a2297e
geonwoo <39722357+geonoo@users.noreply.github.com>
2022-02-05 16:24:49 +0900

    퀵정렬

---
+++ src/doit/chap06/QuickSortQ12.java
@@ -0,0 +1,59 @@
+package doit.chap06;
+
+import java.util.Scanner;
+
+public class QuickSortQ12 {
+	
+	static void swap(int[] a, int idx1, int idx2) {
+		int t = a[idx1]; a[idx1] = a[idx2]; a[idx2] = t;
+	}
+	
+	static void quickSort(int[] a, int left, int right) {
+		int pl = left;
+		int pr = right;
+		int x = a[(pl + pr) / 2];
+		
+		System.out.printf("a[%d]~a[%d] : {", left, right);
+		for (int i = left; i < right; i++) {
+			System.out.printf("%d, ", a[i]);
+		}
+		System.out.printf("%d}\n", a[right]);
+		
+		do {
+			while (a[pl] < x) pl++;
+			while (a[pr] > x) pr--;
+			if(pl <= pr)
+				swap(a, pl++, pr--);
+		}while(pl <= pr);
+		
+		if((x - pl) > (pr - x)) {
+			if(left < pr) quickSort(a, left, pr);
+			if(pl < right) quickSort(a, pl, right);
+		}else {
+			if(pl < right) quickSort(a, pl, right);
+			if(left < pr) quickSort(a, left, pr);
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.println("퀵정");
+		System.out.print("요솟수：");
+		int nx = stdIn.nextInt();
+		int[] x = new int[nx];
+
+		for (int i = 0; i < nx; i++) {
+			System.out.print("x[" + i + "]：");
+			x[i] = stdIn.nextInt();
+		}
+
+		quickSort(x, 0, nx-1); // 배열 x를 단순선택정렬
+		System.out.println("오름차순 정렬 ok");
+		for (int i = 0; i < x.length; i++) {
+			System.out.print(x[i] + " ");
+		}
+	}
+
+}
---
+++ src/doit/chap06/QuickSortVerboseQ12.java
@@ -0,0 +1,63 @@
+package doit.chap06;
+import java.util.Scanner;
+
+import doit.chap04.IntStack;
+
+class QuickSortVerboseQ12 {
+	// 배열의 요소 a[idx1]과 a[idx2]를 교환
+	static void swap(int[] a, int idx1, int idx2) {
+		int t = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = t;
+	}
+
+	// 퀵정렬(비재귀버전)
+	static void quickSort(int[] a, int left, int right) {
+		IntStack lstack = new IntStack(right - left + 1);
+		IntStack rstack = new IntStack(right - left + 1);
+
+		while (lstack.isEmpty() != true) {
+			int pl = left = lstack.pop(); // 왼쪽 커서
+			int pr = right = rstack.pop(); // 오른쪽 커서
+			int x = a[(left + right) / 2]; // 피벗은 중앙의 요소
+
+			do {
+				while (a[pl] < x)
+					pl++;
+				while (a[pr] > x)
+					pr--;
+				if (pl <= pr)
+					swap(a, pl++, pr--);
+			} while (pl <= pr);
+
+			if (left < pr) {
+				lstack.push(left); // 머리쪽 그룹의 범위
+				rstack.push(pr); // (index)를 푸시
+			}
+			if (pl < right) {
+				lstack.push(pl); // 꼬리쪽그룹의 범위
+				rstack.push(right); // (index)를 푸시
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.println("퀵정렬 ");
+		System.out.print("요솟수：");
+		int nx = stdIn.nextInt();
+		int[] x = new int[nx];
+
+		for (int i = 0; i < nx; i++) {
+			System.out.print("x[" + i + "]：");
+			x[i] = stdIn.nextInt();
+		}
+
+		quickSort(x, 0, nx - 1); // 배열 x를 퀵정렬
+
+		System.out.println("오름차순으로 정렬했습니다.");
+		for (int i = 0; i < nx; i++)
+			System.out.println("x[" + i + "] = " + x[i]);
+	}
+}

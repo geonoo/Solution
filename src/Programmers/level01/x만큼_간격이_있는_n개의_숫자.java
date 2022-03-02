7974338bd4587ec7194a469a8fac6aef27b9713d
geonwoo <39722357+geonoo@users.noreply.github.com>
2022-03-02 09:40:15 +0900

    최종변경!
    매개변수 그대로 두기

--- src/Programmers/level01/x만큼_간격이_있는_n개의_숫자.java
+++ src/Programmers/level01/x만큼_간격이_있는_n개의_숫자.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class x만큼_간격이_있는_n개의_숫자 {
 	
 	public static void main(String[] args) {
-		System.out.println(Arrays.toString(solution2(10000000, 2000)));
+		System.out.println(Arrays.toString(solution2(10000000, 1000)));
 	}
 	
 	static public long[] solution(int x, int n) {
@@ -25,11 +25,11 @@ public class x만큼_간격이_있는_n개의_숫자 {
 //                     1,410,065,408
 //    –2,147,483,648 ~ 2,147,483,647
 //    int 시 범위 오류
-    static public long[] solution2(int x, int n ){
+    static public long[] solution(int x, int n ){
         long[] answer = new long[n];
 
         for (int i = 0; i < n; i++) {
-            answer[i] = x * i + x;
+            answer[i] = (long)x * i + x;
         }
 
         return answer;

1a2bc7048d25aeb7f94542901b1f0d93bbfe8134
geonwoo <39722357+geonoo@users.noreply.github.com>
2022-03-04 13:42:12 +0900

    하샤드 수
    tistory > StringBufferTest
    Programers > SwitchExam
    Programers > 하샤드 수

--- src/Programmers/level01/하샤드_수.java
+++ src/Programmers/level01/하샤드_수.java
@@ -3,7 +3,7 @@ package Programmers.level01;
 public class 하샤드_수 {
 		
     public static void main(String[] args) {
-    	System.out.println(solution(216));
+    	System.out.println(solution2(216));
     }
     
     static public boolean solution(int x) {
@@ -24,6 +24,19 @@ public class 하샤드_수 {
     	
     	return answer;
     }
+//	10	true
+//	12	true
+//	11	false
+//	13	false
+	static public boolean solution2(int x) {
+		String s = x + "";
+		int total = 0;
+		for (int i = 0; i < s.length(); i++) {
+			total += Integer.parseInt(s.charAt(i)+"");
+		}
+
+		return x % total == 0;
+	}
 	    
 	    
 }

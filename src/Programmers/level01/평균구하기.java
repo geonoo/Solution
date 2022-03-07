d9719180322a18d62ca055c0fe879bf995ec2c66
geonwoo <39722357+geonoo@users.noreply.github.com>
2022-03-07 19:50:21 +0900

    level1
    평균구하기

--- src/Programmers/level01/평균구하기.java
+++ src/Programmers/level01/평균구하기.java
@@ -3,8 +3,8 @@ package Programmers.level01;
 public class 평균구하기 {
 		
    public static void main(String[] args) {
-    	int[] arr = {5,5};
-    	System.out.println(solution(arr));
+    	int[] arr = {1,2,3,4};
+    	System.out.println(solution2(arr));
     }
     
     static public double solution(int[] arr) {
@@ -17,6 +17,16 @@ public class 평균구하기 {
         answer = (double)sum/(double)arr.length; 
         
         return answer;
+    }
+
+    static public double solution2(int[] arr) {
+       double answer = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
+
+       return answer/arr.length;
     }
 	    
 	    

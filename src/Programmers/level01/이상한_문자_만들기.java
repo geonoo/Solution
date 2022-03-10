83faf12f0f804c06ec44590ca2c6871b5f1bbcab
geonwoo <39722357+geonoo@users.noreply.github.com>
2022-03-11 04:11:23 +0900

    level1
    이상한 문자 만들기
    자릿수 더하기

--- src/Programmers/level01/이상한_문자_만들기.java
+++ src/Programmers/level01/이상한_문자_만들기.java
@@ -1,16 +1,18 @@
 package Programmers.level01;
 
+import java.util.Locale;
+
 public class 이상한_문자_만들기 {
     public static void main(String[] args) {
     	//TrY HeLlO WoRlD
-    	System.out.println(solution("try hello world"));
+    	System.out.println(solution("  aa ss"));
     }
     
     static public String solution(String s) {
         String answer = "";
         
         String[] str = s.split("");
-        
+
         int idx = 0;
         for (int i = 0; i < str.length; i++) {
 			if(str[i].equals(" ")) {
@@ -24,11 +26,8 @@ public class 이상한_문자_만들기 {
 				idx++;
 			}
 		}
-        
-        
         return answer;
     }
-	    
 	    
 }
 	
--- src/Programmers/level01/자릿수_더하기.java
+++ src/Programmers/level01/자릿수_더하기.java
@@ -3,7 +3,7 @@ package Programmers.level01;
 public class 자릿수_더하기 {
 		
     public static void main(String[] args) {
-    	System.out.println(solution(5555));
+    	System.out.println(solution2(5555));
     }
     
     static public int solution(int n) {
@@ -16,7 +16,16 @@ public class 자릿수_더하기 {
         
         return answer;
     }
-	    
+
+    static public int solution2(int n) {
+        int answer = 0;
+        String str = n + "";
+        for (int i = 0; i < str.length(); i++) {
+            answer += str.charAt(i) - '0';
+        }
+
+        return answer;
+    }
 	    
 }
 	

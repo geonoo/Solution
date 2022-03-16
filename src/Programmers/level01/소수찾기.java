30af2b351797f1146433236af39b2648d5b9a688
geonwoo <39722357+geonoo@users.noreply.github.com>
2022-03-16 11:20:12 +0900

    프로그래머스
    소수 찾기

--- src/Programmers/level01/소수찾기.java
+++ src/Programmers/level01/소수찾기.java
@@ -5,7 +5,7 @@ public class 소수찾기 {
     public static void main(String[] args) {
     	long beforeTime = System.currentTimeMillis(); 
         
-    	System.out.println("시작 : "+solution(10000000));
+    	System.out.println("시작 : "+solution2(10000000));
     	        
     	long afterTime = System.currentTimeMillis();
     	long secDiffTime = (afterTime - beforeTime);
@@ -37,6 +37,21 @@ public class 소수찾기 {
         }
         
         return answer;
+    }
+
+    // 4, 6, 8, 10 ... 배수를 제거
+    static public int solution2(int n) {
+        int anwser = 0;
+        boolean[] arr = new boolean[n+1];
+
+        for (int i = 2; i*i <= n ; i++)
+            if(!arr[i])
+                for (int j = i+i; j <= n ; j+=i) arr[j] = true;
+
+        for (int i = 2; i <= n ; i++)
+            if(!arr[i]) anwser++;
+
+        return anwser;
     }
 	    
 }
--- src/baekjoon/s1000/s1110.java
+++ src/baekjoon/s1000/s1110.java
@@ -3,19 +3,21 @@ package baekjoon.s1000;
 import java.util.Scanner;
 
 public class s1110 {
+//    26부터 시작한다. 2+6 = 8이다. 새로운 수는 68이다.
+//    6+8 = 14이다. 새로운 수는 84이다.
+//    8+4 = 12이다. 새로운 수는 42이다.
+//    4+2 = 6이다. 새로운 수는 26
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int num = sc.nextInt();
-        int temp = (num / 10) + (num % 10);
-        int tNum = temp + (num % 10);
-
-        System.out.println(tNum);
-
-        int rNum = (temp % 10) + (tNum % 10);
-        System.out.println(rNum);
-        int aNum = (tNum % 10) + (rNum % 10);
-        System.out.println(rNum);
+        int one = (num % 10)*10 + (((num / 10) + (num % 10)) % 10);
+        int cnt = 1;
+        while (num != one){
+            one = (one % 10)*10 + (((one / 10) + (one % 10)) % 10);
+            cnt++;
+        }
+        System.out.println(cnt);
 
     }
 }
